171fbbd124d07a230b54bc62f199172dcf1b2278
Jose Castro <drakonc@users.noreply.github.com>
2019-09-04 18:40:23 -0500

    Agregando UsuarioModile En el Import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioModule } from './usuario/usuario.module';
 import { LoginModule } from './login/login.module';
 import { HomeModule } from './home/home.module';
 import { RolService } from './usuario/rol/rol.service';
 
 @NgModule({
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule, AppRoutingModule, HttpClientModule, HomeModule, LoginModule,FormsModule ],
+	imports: [ BrowserModule, AppRoutingModule, HttpClientModule, HomeModule, UsuarioModule, LoginModule, FormsModule ],
 	providers: [ RolService ],
 	bootstrap: [ AppComponent ]
 })
